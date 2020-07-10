03fc378c7af34bad460cf4d69d125d200eadf7b4
Roberto Zaniboni <zaniboni@gmail.com>
2020-07-10 13:42:29 +0200

    fix Home controller

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -3,22 +3,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace WishList {
-    public class Startup {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddMvc();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-            } else {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseRouting();
-            app.UseEndpoints(endpoints => {endpoints.MapDefaultControllerRoute(); });
-        }
+namespace WishList
+{
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
     }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+      app.UseRouting();
+      app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+    }
+  }
 }
