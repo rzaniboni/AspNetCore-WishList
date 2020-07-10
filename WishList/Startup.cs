c539d5ed51ec3ba0dbfcd8da0e55d47db5bf5221
Roberto Zaniboni <zaniboni@gmail.com>
2020-07-10 13:28:51 +0200

    fix space

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -11,12 +11,11 @@ namespace WishList {
             services.AddMvc();
         }
 
-
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             } else {
-                app.UseExceptionHandler ("/Home/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
             app.UseEndpoints(endpoints => {endpoints.MapDefaultControllerRoute(); });
